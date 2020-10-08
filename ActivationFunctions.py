1b03ab4af34820c920f018b23c2408d68a6bea32
tom-fougere <fougeretom@gmail.com>
2020-10-08 16:07:45 +0200

    Create ActivationFunctions.py
    Add sigmoid function and its derivative

---
+++ ActivationFunctions.py
@@ -0,0 +1,29 @@
+from numpy import exp
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+    Y = 1 / (1 + exp(-x))
+    Arguments:
+    z -- A scalar or numpy array of any size
+    Return:
+    s -- sigmoid(x)
+    """
+
+    s = 1 / (1 + np.exp(-x))
+    
+    return s
+    
+def sigmoid_grad(x):
+    """
+    Compute the derivative of sigmoid of x
+    d(sigmoid)/dx = sigmoid(x) * (1 - sigmoid(x))
+    Arguments:
+    z -- A scalar or numpy array of any size
+    Return:
+    s -- d_sigmoid(x)/dx
+    """
+
+    s = simoid(x) * (1 - sigmoid(x))
+    
+    return s
